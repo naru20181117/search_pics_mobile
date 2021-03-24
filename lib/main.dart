da130fa6bdbdde2c22f44a8d13ba1a710b11fef7
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-03-25 00:39:14 +0900

    reset: second page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+
 import 'transtion_top.dart';
-import 'transtion_second.dart';
 // import 'transtion_third.dart';
 
 void main() {
@@ -10,13 +10,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => new TranstionTop(),
-        '/second': (context) => new TranstionSecond(),
-        // '/third': (context) => new TranstionThird(),
-      }
-    );
+    return MaterialApp(initialRoute: '/', routes: {
+      '/': (context) => new TranstionTop(),
+      // '/second': (context) => new TranstionSecond(),
+      // '/third': (context) => new TranstionThird(),
+    });
   }
 }
