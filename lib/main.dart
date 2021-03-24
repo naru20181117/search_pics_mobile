68ad6eb2c43d3398f7daf5fa5803ad43219afd7c
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-03-24 10:27:32 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'transtion_top.dart';
+import 'transtion_second.dart';
+// import 'transtion_third.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => new TranstionTop(),
+        '/second': (context) => new TranstionSecond(),
+        // '/third': (context) => new TranstionThird(),
+      }
+    );
+  }
+}
