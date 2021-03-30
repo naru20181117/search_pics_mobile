fd15c9a9b09416b3d599824e1d1570beaea71018
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-03-31 00:40:30 +0900

    set: Third page

--- lib/transtion_third.dart
+++ lib/transtion_third.dart
@@ -3,6 +3,35 @@ import 'package:flutter/material.dart';
 class TranstionThird extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold();
+    return new Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: new AppBar(
+        title: const Text(
+          'Flutter Third Screen',
+          style: TextStyle(color: Colors.white, fontSize: 20.0),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: new Center(
+        child: new SizedBox(
+          width: 200.0,
+          height: 80.0,
+          child: new RaisedButton(
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0)),
+            child: Text(
+              'Return Top',
+              style: TextStyle(fontSize: 30.0),
+            ),
+            color: Colors.green,
+            onPressed: () {
+              Navigator.popUntil(context, ModalRoute.withName('/'));
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
